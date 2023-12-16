4f5f7e2243ac413534f12f68356714f46315d7de
Saqlain Abid <86692833+Saqlain143@users.noreply.github.com>
2023-12-16 20:42:14 +0500

    Update Day1.py

--- 2023/Day1.py
+++ 2023/Day1.py
@@ -1,1 +1,11 @@
-https://colab.research.google.com/drive/1s1XuXon9y1JHTTI4JeeAPquT18Eve1am?authuser=1#scrollTo=68OIxcD9iucZ&line=3&uniqifier=1
+def run_part1():
+  with open("input.txt", "r") as file:
+    lines = file.readlines()
+    total_sum = 0
+    for line in lines:
+      digits = [int(char) for char in line if char.isdigit()]
+      number = int(f"{digits[0]}{digits[-1]}")
+      total_sum += number
+  return total_sum
+
+print(run_part1())
